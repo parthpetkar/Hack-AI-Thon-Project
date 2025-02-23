0a39d6e84be37166a99251a52f758f1f8964d9d4
Parth Kalani <parthkalani1899@gmail.com>
2025-02-24 00:18:53 +0530

    updated requirements

--- Backend/app.py
+++ Backend/app.py
@@ -28,7 +28,7 @@ app.register_blueprint(admin_bp, url_prefix='/admin')
 
 @app.route('/')
 def home():
-    return "Flask app is running with Neo4j!"
+    return "Flask app is running with Neo4j on Render!"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=10000)
